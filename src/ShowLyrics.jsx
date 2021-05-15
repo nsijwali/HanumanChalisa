d31610dc7bcd904eb670c92d5d376a59abafba2f
Nitin Sijwali <NITIN.SIJWALI@questdot.io>
2021-05-15 19:48:41 +0530

    added languages

--- src/ShowLyrics.jsx
+++ src/ShowLyrics.jsx
@@ -1,16 +1,41 @@
+import { useState, useEffect } from 'react';
 import lyrics from './lyrics';
 import './App.css';
 
 const ShowLyrics = () => {
-	const localLyrics = lyrics.split('।।');
+	const [doha, setDoha] = useState([]);
+	const [chopai, setChopai] = useState([]);
+	const [lang, setlang] = useState('english');
+	useEffect(() => {
+		setDoha(lyrics?.[lang]?.doha.split('.'));
+		setChopai(lyrics?.[lang]?.chopai.split('.'));
+	}, [lang]);
+
+	const handleLangChange = (e) => {
+		setlang(e.target.value);
+	};
+
 	return (
 		<>
-			{localLyrics.map((para) => (
-				<div className='chopai'>
-					<p>{para.split('।')[0]}</p>
-					<p>{para.split('।')[1]}</p>
-				</div>
+			<h3>Doha</h3>
+			{doha.map((para) => (
+				<pre className='chopai'>
+					<p>{para.split(',')[0]}</p>
+					<p>{para.split(',')[1]}</p>
+				</pre>
 			))}
+			<h3>Chopai</h3>
+			{chopai.map((para) => (
+				<pre className='chopai'>
+					<p>{para.split(',')[0]}</p>
+					<p>{para.split(',')[1]}</p>
+				</pre>
+			))}
+
+			<select className='select__lang' onChange={handleLangChange} value={lang}>
+				<option value='hindi'>Hindi</option>
+				<option value='english'>English</option>
+			</select>
 		</>
 	);
 };
--- src/lyrics.js
+++ src/lyrics.js
@@ -1,119 +1,288 @@
-module.exports = `जय हनुमान ज्ञान गुन सागर।
-जय कपीस तिहुं लोक उजागर।।
+module.exports = {
+	hindi: {
+		doha: `श्रीगुरु चरन सरोज रज, निज मनु मुकुरु सुधारि.
+बरनऊं रघुबर बिमल जसु, जो दायकु फल चारि.`,
+		chopai: `जय हनुमान ज्ञान गुन सागर,
+जय कपीस तिहुं लोक उजागर.
  
-रामदूत अतुलित बल धामा।
-अंजनि-पुत्र पवनसुत नामा।।
+रामदूत अतुलित बल धामा,
+अंजनि-पुत्र पवनसुत नामा.
  
-महाबीर बिक्रम बजरंगी।
-कुमति निवार सुमति के संगी।।
+महाबीर बिक्रम बजरंगी,
+कुमति निवार सुमति के संगी.
  
-कंचन बरन बिराज सुबेसा।
-कानन कुंडल कुंचित केसा।।
+कंचन बरन बिराज सुबेसा,
+कानन कुंडल कुंचित केसा.
  
-हाथ बज्र औ ध्वजा बिराजै।
-कांधे मूंज जनेऊ साजै।।
+हाथ बज्र औ ध्वजा बिराजै,
+कांधे मूंज जनेऊ साजै.
  
-संकर सुवन केसरीनंदन।
-तेज प्रताप महा जग बन्दन।।
+संकर सुवन केसरीनंदन,
+तेज प्रताप महा जग बन्दन.
  
-विद्यावान गुनी अति चातुर।
-राम काज करिबे को आतुर।।
+विद्यावान गुनी अति चातुर,
+राम काज करिबे को आतुर.
  
-प्रभु चरित्र सुनिबे को रसिया।
-राम लखन सीता मन बसिया।।
+प्रभु चरित्र सुनिबे को रसिया,
+राम लखन सीता मन बसिया.
  
-सूक्ष्म रूप धरि सियहिं दिखावा।
-बिकट रूप धरि लंक जरावा।।
+सूक्ष्म रूप धरि सियहिं दिखावा,
+बिकट रूप धरि लंक जरावा.
  
-भीम रूप धरि असुर संहारे।
-रामचंद्र के काज संवारे।।
+भीम रूप धरि असुर संहारे,
+रामचंद्र के काज संवारे.
  
-लाय सजीवन लखन जियाये।
-श्रीरघुबीर हरषि उर लाये।।
+लाय सजीवन लखन जियाये,
+श्रीरघुबीर हरषि उर लाये.
  
-रघुपति कीन्ही बहुत बड़ाई।
-तुम मम प्रिय भरतहि सम भाई।।
+रघुपति कीन्ही बहुत बड़ाई,
+तुम मम प्रिय भरतहि सम भाई.
  
-सहस बदन तुम्हरो जस गावैं।
-अस कहि श्रीपति कंठ लगावैं।।
+सहस बदन तुम्हरो जस गावैं,
+अस कहि श्रीपति कंठ लगावैं.
  
-सनकादिक ब्रह्मादि मुनीसा।
-नारद सारद सहित अहीसा।।
+सनकादिक ब्रह्मादि मुनीसा,
+नारद सारद सहित अहीसा.
  
-जम कुबेर दिगपाल जहां ते।
-कबि कोबिद कहि सके कहां ते।।
+जम कुबेर दिगपाल जहां ते,
+कबि कोबिद कहि सके कहां ते.
  
-तुम उपकार सुग्रीवहिं कीन्हा।
-राम मिलाय राज पद दीन्हा।।
+तुम उपकार सुग्रीवहिं कीन्हा,
+राम मिलाय राज पद दीन्हा.
  
-तुम्हरो मंत्र बिभीषन माना।
-लंकेस्वर भए सब जग जाना।।
+तुम्हरो मंत्र बिभीषन माना,
+लंकेस्वर भए सब जग जाना.
  
-जुग सहस्र जोजन पर भानू।
-लील्यो ताहि मधुर फल जानू।।
+जुग सहस्र जोजन पर भानू,
+लील्यो ताहि मधुर फल जानू.
  
-प्रभु मुद्रिका मेलि मुख माहीं।
-जलधि लांघि गये अचरज नाहीं।।
+प्रभु मुद्रिका मेलि मुख माहीं,
+जलधि लांघि गये अचरज नाहीं.
  
-दुर्गम काज जगत के जेते।
-सुगम अनुग्रह तुम्हरे तेते।।
+दुर्गम काज जगत के जेते,
+सुगम अनुग्रह तुम्हरे तेते.
  
-राम दुआरे तुम रखवारे।
-होत न आज्ञा बिनु पैसारे।।
+राम दुआरे तुम रखवारे,
+होत न आज्ञा बिनु पैसारे.
  
-सब सुख लहै तुम्हारी सरना।
-तुम रक्षक काहू को डर ना।।
+सब सुख लहै तुम्हारी सरना,
+तुम रक्षक काहू को डर ना.
  
-आपन तेज सम्हारो आपै।
-तीनों लोक हांक तें कांपै।।
+आपन तेज सम्हारो आपै,
+तीनों लोक हांक तें कांपै.
  
-भूत पिसाच निकट नहिं आवै।
-महाबीर जब नाम सुनावै।।
+भूत पिसाच निकट नहिं आवै,
+महाबीर जब नाम सुनावै.
  
-नासै रोग हरै सब पीरा।
-जपत निरंतर हनुमत बीरा।।
+नासै रोग हरै सब पीरा,
+जपत निरंतर हनुमत बीरा.
  
-संकट तें हनुमान छुड़ावै।
-मन क्रम बचन ध्यान जो लावै।।
+संकट तें हनुमान छुड़ावै,
+मन क्रम बचन ध्यान जो लावै.
  
-सब पर राम तपस्वी राजा।
-तिन के काज सकल तुम साजा।।
+सब पर राम तपस्वी राजा,
+तिन के काज सकल तुम साजा.
  
-और मनोरथ जो कोई लावै।
-सोइ अमित जीवन फल पावै।।
+और मनोरथ जो कोई लावै,
+सोइ अमित जीवन फल पावै.
  
-चारों जुग परताप तुम्हारा।
-है परसिद्ध जगत उजियारा।।
+चारों जुग परताप तुम्हारा,
+है परसिद्ध जगत उजियारा.
  
-साधु-संत के तुम रखवारे।
-असुर निकंदन राम दुलारे।।
+साधु-संत के तुम रखवारे,
+असुर निकंदन राम दुलारे.
  
-अष्ट सिद्धि नौ निधि के दाता।
-अस बर दीन जानकी माता।।
+अष्ट सिद्धि नौ निधि के दाता,
+अस बर दीन जानकी माता.
  
-राम रसायन तुम्हरे पासा।
-सदा रहो रघुपति के दासा।।
+राम रसायन तुम्हरे पासा,
+सदा रहो रघुपति के दासा.
  
-तुम्हरे भजन राम को पावै।
-जनम-जनम के दुख बिसरावै।।
+तुम्हरे भजन राम को पावै,
+जनम-जनम के दुख बिसरावै.
  
-अन्तकाल रघुबर पुर जाई।
-जहां जन्म हरि-भक्त कहाई।।
+अन्तकाल रघुबर पुर जाई,
+जहां जन्म हरि-भक्त कहाई.
  
-और देवता चित्त न धरई।
-हनुमत सेइ सर्ब सुख करई।।
+और देवता चित्त न धरई,
+हनुमत सेइ सर्ब सुख करई.
  
-संकट कटै मिटै सब पीरा।
-जो सुमिरै हनुमत बलबीरा।।
+संकट कटै मिटै सब पीरा,
+जो सुमिरै हनुमत बलबीरा.
  
-जै जै जै हनुमान गोसाईं।
-कृपा करहु गुरुदेव की नाईं।।
+जै जै जै हनुमान गोसाईं,
+कृपा करहु गुरुदेव की नाईं.
  
-जो सत बार पाठ कर कोई।
-छूटहि बंदि महा सुख होई।।
+जो सत बार पाठ कर कोई,
+छूटहि बंदि महा सुख होई.
  
-जो यह पढ़ै हनुमान चालीसा।
-होय सिद्धि साखी गौरीसा।।
+जो यह पढ़ै हनुमान चालीसा,
+होय सिद्धि साखी गौरीसा.
  
-तुलसीदास सदा हरि चेरा।
-कीजै नाथ हृदय मंह डेरा।।`;
+तुलसीदास सदा हरि चेरा,
+कीजै नाथ हृदय मंह डेरा.`,
+	},
+	english: {
+		doha: `Sri Guru Charan Saroj Raj Nij Man Mukur Sudhari, Baranau Raghuvar Bimal Jasu Jo Dayaku Ohal Chari.
+
+Buddhiheen Tanu Janike Sumarau Pavan Kumar, Bal Buddhi Vidya Dehu Mohi Harau Kalesh Vikar.`,
+		chopai: `Jai Hanuman gyan gun sagar,
+Jai Kapis tihun lok ujagar.
+
+Ram doot atulit bal dhama,
+
+Anjaani putra Pavan sut nama.
+
+Mahabir Bikram Bajrangi,
+
+Kumati nivar sumati ke sangi.
+
+Kanchan varan viraj subesa,
+
+Kanan kundal kunchit kesa.
+
+Hath vajra aur dhvaja viraje,
+
+Kaandhe moonj janeyu saje.
+
+Shankar suvan kesri nandan,
+
+Tej pratap maha jag vandan.
+
+Vidyavan guni ati chatur,
+
+Ram kaj karibe ko aatur.
+
+Prabu charitra sunibe ko rasiya,
+
+Ram Lakhan Sita man basiya.
+
+Sukshma roop dhari siyahi dikhava,
+
+Vikat roop dhari lank jarava.
+
+Bhima roop dhari asur sanghare,
+
+Ramachandra ke kaj sanvare.
+
+Laye Sanjivan Lakhan jiyaye,
+
+Shri Raghuvir harashi ur laye.
+
+Raghupati kinhi bahut badai,
+
+Tum mam priye Bharat hi sam bhai.
+
+Sahas badan tumharo yash gaave,
+
+Asa kahi Shripati kanth lagaave.
+
+Sankadhik Brahmadi Muneesa,
+
+Narad Saarad sahit Aheesa.
+
+Yam Kuber Digpaal jahan te,
+
+Kavi Kovid kahi sake kahan te.
+
+Tum upkar Sugreevahin keenha,
+
+Ram milaye rajpad deenha.
+
+Tumharo mantra Vibheeshan maana,
+
+Lankeshwar bhaye sab jag jana.
+
+Yug sahastra jojan par Bhanu,
+
+Leelyo tahi madhur phal janu.
+
+Prabhu mudrika meli mukh mahee,
+
+Jaladhi langhi gaye achraj nahee.
+
+Durgam kaj jagath ke jete,
+
+Sugam anugraha tumhre tete.
+
+Ram dwaare tum rakhvare,
+
+Hoat na agya binu paisare.
+
+Sub sukh lahae tumhari sar na,
+
+Tum rakshak kahu ko dar naa.
+
+Aapan tej samharo aapai,
+
+Teenhon lok hank te kanpai.
+
+Bhoot pisaach nikat nahin aavai,
+
+Mahavir jab naam sunave.
+
+Nase rog harae sab peera,
+
+Japat nirantar Hanumant beera.
+
+Sankat te Hanuman chudavae,
+
+Man kram vachan dhyan jo lavai.
+
+Sab par Ram tapasvee raja,
+
+Tin ke kaj sakal Tum saja.
+
+Aur manorath jo koi lavai,
+
+Soi amit jeevan phal pavai.
+
+Charon jug partap tumhara,
+
+Hai persidh jagat ujiyara.
+
+Sadhu Sant ke tum rakhware,
+
+Asur nikandan Ram dulhare.
+
+Ashta sidhi nav nidhi ke dhata,
+
+As var deen Janki mata.
+
+Ram rasayan tumhare pasa,
+
+Sada raho Raghupati ke dasa.
+
+Tumhare bhajan Ram ko pavai,
+
+Janam janam ke dukh bisraavai.
+
+Anth kaal Raghuvir pur jayee,
+
+Jahan janam Hari Bakht Kahayee.
+
+Aur Devta chit na dharahi,
+
+Hanumanth sehi sarve sukh karehi.
+
+Sankat kate mite sab peera,
+
+Jo sumirai Hanumat balbeera.
+
+Jai Jai Jai Hanuman Gosayin,
+
+Kripa karahu Gurudev ki nyahin.
+
+Jo sat bar path kare kohi,
+
+Chutahi bandhi maha sukh hohi.
+
+Jo yah padhe Hanuman Chalisa,
+
+Hoye siddhi sakhi Gaurisa.
+
+Tulsidas sada hari chera,
+
+Keejai nath hridaye mein dera.`,
+	},
+};
